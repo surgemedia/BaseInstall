4105ff7de1bfd0ea7d393b8d3ccbcfb8d3576e0f
Vani <surge@Desktop-10s-iMac.local>
2015-09-09 13:37:05 +1000

    added script to enable multi level navigation

--- wp-content/themes/Basetheme/assets/scripts/main.js
+++ wp-content/themes/Basetheme/assets/scripts/main.js
@@ -74,4 +74,14 @@
   // Load Events
   $(document).ready(UTIL.loadEvents);
 
+  /*=============================================
+  = Enabling multi-level navigation =
+  ===============================================*/
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+      event.preventDefault(); 
+      event.stopPropagation(); 
+      $(this).parent().siblings().removeClass('open');
+      $(this).parent().toggleClass('open');
+  });
+
 })(jQuery); // Fully reference jQuery after this point.

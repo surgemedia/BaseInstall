89646d6af1858d32327518da16eabfe1685418f3
Alex <surge@aegirs-iMac.local>
2015-11-06 10:45:17 +1000

    Setup Youtube Background

--- wp-content/themes/Basetheme/assets/scripts/main.js
+++ wp-content/themes/Basetheme/assets/scripts/main.js
@@ -68,11 +68,38 @@
 
       // Fire common finalize JS
       UTIL.fire('common', 'finalize');
-    }
+    } 
   };
 
   // Load Events
   $(document).ready(UTIL.loadEvents);
+/*================================
+=            Carousel            =
+================================*/
+$('.carousel').carousel({
+  interval: 5000,
+  pause: false
+})
+
+/*==========================================
+=            Youtube background            =
+==========================================*/
+$('#video-bg').YTPlayer({
+  videoId: $('#video-bg').data('video-id'),
+   playerVars: {
+    modestbranding: 0,
+    autoplay: 1,
+    controls: 0,
+    showinfo: 0,
+    wmode: 'transparent',
+    branding: 0,
+    rel: 0,
+    autohide: 0
+  },
+  callback: function() {
+    console.log($('#video-bg').data('video-id'));
+  }
+  });   
 
   /*=============================================
   = Enabling multi-level navigation =
--- wp-content/themes/Basetheme/templates/footer.php
+++ wp-content/themes/Basetheme/templates/footer.php
@@ -1,5 +1,30 @@
-<footer class="content-info" role="contentinfo">
+<footer id="footer" class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  	<div class="col-lg-12 text-center"><img src="" alt="Surge Media"></div>
+    <div class="col-lg-4">
+    <span class="title"></span>
+    <ul>
+    	<li></li>
+    	<li></li>
+    	<li></li>
+    	<li></li>
+    </ul>
+    <small>Terms &amp; Conditions |</small>
+    <small> Privacy Policy</small>
+     </div>
+    <div class="col-lg-6">
+    	<span class="title">Sign Up for our newsletter. It's awesome</span>
+    	<!-- form goes here -->
+    </div>
+    <div class="col-lg-4">
+    	<span class="title text-center">Follow Us</span>
+    	<ul>
+    		<li><a href=""><i></i></a></li>
+    		<li><a href=""><i></i></a></li>
+    		<li><a href=""><i></i></a></li>
+    		<li><a href=""><i></i></a></li>
+    		<li><a href=""><i></i></a></li>
+    	</ul>
+    </div>
   </div>
 </footer>

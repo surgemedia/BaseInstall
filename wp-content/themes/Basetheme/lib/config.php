5f2327455f7a7d08fab7fcfa9c33187ef66a194d
Alex <surge@aegirs-iMac.local>
2015-11-06 10:46:22 +1000

    Home Page - New Styling - Added Global for h1 and p - Variables setup - Deleted old Post types

--- wp-content/themes/Basetheme/lib/config.php
+++ wp-content/themes/Basetheme/lib/config.php
@@ -54,7 +54,9 @@ function display_sidebar() {
       [
         'is_404',
         'is_front_page',
-        ['is_page_template', 'template-custom.php']
+        'is_page',
+        'is_single',
+
       ]
     );
 

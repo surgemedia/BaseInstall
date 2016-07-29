5a337b12c5808b2a87ce205bb6127f8ddb472666
Hector <hectornucv@gmail.com>
2016-07-29 15:38:47 +1000

    Core upgrade
    new function get options for sections 
    json acfs
    components

--- wp-content/themes/surge-starter/components/atom/button-list.php
+++ wp-content/themes/surge-starter/components/atom/button-list.php
@@ -1,7 +1,34 @@
-<ul class="button-list">
-	<?php foreach ($vars['button_list'] as $button) {?>
-	<li class="<?php echo $button['class'] ?>">
-		<a href="<?php echo $button['url'] ?>"> <?php echo $button['text']; ?> </a>
-	</li>
+<?php 
+ // debug($vars);
+if (is_array($vars)) {
+$button_size= sizeof($vars);
+if (1<$button_size) : ?>
+<div class="button-list">
+<?php endif; ?>
+	<?php foreach ($vars as $key=>$button) {?>
+		<?php switch ($button['link_type']) {
+			case 'internal':
+				$link = (!empty($button['internal_link'])) ? $button['internal_link'] : '#no-link';
+				break;
+			case 'external':
+				$link = (!empty($button['external_link'])) ? $button['external_link'] : '#no-link';
+				break;
+			case 'anchor':
+				$link = "#".(!empty($button['anchor_link'])) ? $button['anchor_link'] : '#no-link';
+				break;
+			case 'archive':
+				$link = "#".(!empty($button['archive_link'])) ? $button['archive_link'] : '#no-link';
+				break;
+			default:
+				$link = (!empty($button['link'])) ? $button['link'] : '#no-link';
+				break;
+		}  
+		if($button['create_other_window'])
+			$button['extra-data'] .=" target='_blank'";
+		?>
+		<a class="btn text-uppercase <?php echo $button['class']?>" href="<?php echo $link?>" <?php echo $button['extra-data'] ?>> <?php echo $button['text']; ?> </a>
 	<?php } ?>
-</ul>
+<?php if (1<$button_size) : ?>
+</div>
+<?php endif; ?>
+<?php } ?>
--- wp-content/themes/surge-starter/page.php
+++ wp-content/themes/surge-starter/page.php
@@ -1,13 +1,71 @@
 <?php
+if (is_front_page()){ ?>
+<?php 
+	get_component([ 'template' => 'organism/homepage-heading',
+											'remove_tags'=> get_field('remove_elements'),
+											'vars' => [
+														"class" => 'padding-4',
+														"element" => get_field('slides'),
+														"category_slides" => get_field('category_slides')
+
+
+														]
+											 ]);
+ ?>
+<?php }else{ ?>
+<?php 
+	get_component([ 'template' => 'organism/page-heading',
+											'remove_tags'=> get_field('remove_elements'),
+											'vars' => [
+														"class" => '',
+														"title" => get_field('title'),
+														"subtitle" => get_field('subtitle'),
+														"content" => get_field('content'),
+														"background" => get_field('background'),
+														"image" => get_field('image'),
+														"button" => get_field('button'),
+
+														]
+											 ]);
+ ?>
+ <?php } ?>
+
+
+<?php
 $layout_builder = get_field('layout');
+//is there block?
+if(isset($layout_builder[0])){
 foreach ($layout_builder as $key => $value) {
 	$section_file = $value['acf_fc_layout'];
+	if(isset($section_file)){
 	unset($value['acf_fc_layout']); //of section
+	
+	$value["section"] = $section_file;
 
+
+	//Call file for display
 			get_component([
 						'template' => 'organism/'.$section_file,
 						'vars' => $value
 			]);
-	unset($section_file);
-}
-?>
+				
+				}
+		unset($section_file);
+	}
+} else {
+	get_component([
+						'template' => 'template/no-section-warning',
+						'vars' => []
+			]);?>
+	<section id="" class="white-bg container padding-4 margin-0 text-left" >
+	 	<article class="col-md-12  molecule card">
+	    <div class="entry-content">
+	      <?php the_content(); ?>
+	    </div>
+	    <footer>
+	      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+	    </footer>
+			</article>
+	</section>	
+<?php }
+ ?>
